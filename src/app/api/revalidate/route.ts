4379234830cc7936c339450e0b9dbebad2d5b10b
akiraonstarknet <zkdice@proton.me>
2025-04-07 15:04:02 +0530

    debug

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -2,15 +2,19 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   const authHeader = request.headers.get('authorization');
-  console.log('Authorization header:', authHeader);
+  console.error('Authorization header:', authHeader);
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     console.error('Unauthorized request');
     return new Response('Unauthorized', {
       status: 401,
     });
   }
-  fetch(`${process.env.HOSTNAME}/api/strategies`);
-  fetch(`${process.env.HOSTNAME}/api/stats`);
+  console.error('Revalidating...', `${process.env.HOSTNAME}/api/strategies`);
+  const prom1 = fetch(`${process.env.HOSTNAME}/api/strategies`);
+  const prom2 = fetch(`${process.env.HOSTNAME}/api/stats`);
+
+  await Promise.all([prom1, prom2]);
+  console.error('Revalidation complete');
   return NextResponse.json(
     {
       revalidated: true,

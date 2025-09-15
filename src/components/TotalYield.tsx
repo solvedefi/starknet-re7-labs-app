af9d170985cdcc4d3a7abd59aef565eb7b5bc425
Eli Karotkin <eli.k@starkware.co>
2025-09-15 17:58:30 +0300

    feat: enhance TotalYield component with layout adjustments and image integration

--- src/components/TotalYield.tsx
+++ src/components/TotalYield.tsx
@@ -1,5 +1,6 @@
-import { Container, Text, VStack } from '@chakra-ui/react';
+import { Container, Flex, Text, VStack, Image } from '@chakra-ui/react';
 import { StrategyDetails } from '@/hooks/useStrategiesInfo';
+import chart from '@public/chart.png';
 
 type TotalYieldProps = {
   strategies: StrategyDetails[];
@@ -14,10 +15,11 @@ export const TotalYield = ({ strategies }: TotalYieldProps) => {
   return (
     <Container
       position="relative"
+      p={0}
       _before={{
         content: '""',
         position: 'absolute',
-        inset: '-2px', // Extend beyond the container for glow
+        inset: '-4px', // Extend beyond the container for glow
         borderRadius: '24px', // Slightly larger than container (14px + 20px)
         background: 'linear-gradient(to right, #2E45D0, #B1525C)',
         filter: 'blur(12px)',
@@ -27,7 +29,7 @@ export const TotalYield = ({ strategies }: TotalYieldProps) => {
     >
       <Container
         position="relative"
-        background="linear-gradient(#1A1919, #1A1919) padding-box, linear-gradient(to right, #2E45D0, #B1525C) border-box"
+        background="linear-gradient(#1D1D1D, #1D1D1D) padding-box, linear-gradient(to right, #2E45D0, #B1525C) border-box"
         border="2px solid transparent"
         width={'100%'}
         h="180px"
@@ -44,10 +46,19 @@ export const TotalYield = ({ strategies }: TotalYieldProps) => {
           justifyContent="space-between"
           alignSelf="stretch"
           position="relative"
+          width={'100%'}
         >
-          <Text fontSize="17px" fontWeight="500">
-            My Total Yield
-          </Text>
+          <Flex justifyContent={'space-between'} width={'100%'}>
+            <Text fontSize="17px" fontWeight="500">
+              My Total Yield
+            </Text>
+            <Image
+              src={chart.src}
+              alt="pfp"
+              width={{ base: '13px', sm: '15px' }}
+              height={{ base: '13px', sm: '15px' }}
+            />
+          </Flex>
           <Text fontSize="42px" fontWeight="600">
             ${(totalYield || 0).toFixed(3)}
           </Text>
